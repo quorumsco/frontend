c615e59de3df65ba4bcce68e1e54d3d6fb79ba9c
Douézan-Grard Guillaume <silverwyrda@gmail.com>
2015-07-30 13:21:26 +0200

    Restart from a sane base

--- static/js/app/fixtures/contacts.js
+++ static/js/app/fixtures/contacts.js
@@ -1,10 +1,29 @@
+var casual = require('chance');
+
+var contact_gen = function(id) {
+  return {
+    id: id,
+    firstname: chance.first(),
+    surname: chance.last(),
+    email: chance.email()
+  };
+};
+
+var array_of = function(times, generator) {
+  var result = [];
+
+  for (var i = 0; i < times; i++) {
+    var contact = generator(i);
+    result.push(contact);
+  }
+
+  return result;
+};
+
 module.exports = function (id) {
   if (id) {
-    return {id: id, firstname: "Jean", surname: "Dupon"};
+    return contact_gen(id);
   } else {
-    return [
-      {id: 0, firstname: "Jean", surname: "Dupond"},
-      {id: 0, firstname: "Jean", surname: "Dupont"}
-    ];
+    return array_of(10, contact_gen);
   }
 };

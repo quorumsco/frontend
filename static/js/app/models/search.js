986621760851536e34ac5a02d52419ab7ff224fb
Poncholay <guillaume.wilmot@epitech.eu>
2015-11-03 10:22:09 +0100

    Forgot this file

---
+++ static/js/app/models/search.js
@@ -0,0 +1,27 @@
+var Emitter = require('events').EventEmitter,
+  search = module.exports = new Emitter(),
+  request = require('superagent'),
+  nocache = require('superagent-no-cache'),
+  // api = "http://localhost:8080",
+  api = "https://api.quorumapps",
+  contact_store = require('./contact_store.js');
+
+search.find = function(query, cb) {
+  query = query.trim();
+  if (query.length == 0) {
+    contact_store.find(cb);
+  } else {
+    request
+    .get(`${api}/search/${query}`)
+    .use(nocache)
+    .withCredentials()
+    .set('Accept', 'application/json')
+    .end(function(err, res) {
+      if (res.body.status === 'success') {
+        cb(res.body.data.contacts);
+      } else {
+        console.log("error");
+      }
+    });
+  }
+}
